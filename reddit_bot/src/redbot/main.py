439cdf2e32c756b6a32d380c00c2d5bd4d3f4eda
Keya Pandia <keyapandia123@gmail.com>
2021-08-08 11:37:09 -0700

    Update main module
    Add a function for ingestion of new posts and another to check hot posts.

--- reddit_bot/src/redbot/main.py
+++ reddit_bot/src/redbot/main.py
@@ -1,28 +1,71 @@
-import pandas as pd
+from datetime import datetime
+import time
+
 import praw
 
-from . import credentials
-from . import db
+import credentials
+import db
 
 
 REDDIT = praw.Reddit(**credentials.load_credentials())
 DATABASE_DEFAULT_PATH = '~/.redbot/redbotdb.sqlite'
+SUB_NAME = 'politics'
+NEW_POST_LIMIT = 20
+HOT_POST_LIMIT = 10
+SLEEP_S = 60
 
-def generate_subreddit_posts_database(limit, sub_name):
+
+def ingest_new_posts(limit, sub_name):
     subreddit = REDDIT.subreddit(sub_name)
-    posts = subreddit.hot(limit=limit)
-    con = db.connect_to_database(DATABASE_DEFAULT_PATH, create_if_empty=True)
+    posts = subreddit.new(limit=limit)
+    con = db.connect_to_db(DATABASE_DEFAULT_PATH, create_if_empty=True)
     uids = db.get_uids(con)
     for num, p in enumerate(posts):
+        now = time.time()
+        duration_s = now - p.created_utc
+        duration_hr = duration_s / 3600.
         if p.id in uids:
-            # update post entry in table
+            if duration_hr <= 1:
+                # Update score of an existing post within 1 hr of its creation.
+                # The score (upvote - downvote) within the first hour of
+                # creation can be used as a predictor for the model.
+                idy = p.id
+                new_score = p.score
+                db.update_score(con, new_score, idy)
+        else:
+            # Create new post entry
+            db.insert_new_post(con, p, None, now, sub_name, None)
+
+    con.commit()
+
+
+def check_hot_posts(limit, sub_name):
+    subreddit = REDDIT.subreddit(sub_name)
+    posts = subreddit.hot(limit=limit)
+    con = db.connect_to_db(DATABASE_DEFAULT_PATH, create_if_empty=True)
+    uids = db.get_uids(con)
+    for num, p in enumerate(posts):
+        now = time.time()
+        duration_s = now - p.created_utc
+        duration_hr = duration_s / 3600.
+        if p.id in uids and duration_hr <= 24:
+            # Update high rank and time corresponding to high rank within
+            # 24 hrs of creation of the post.
             idy = p.id
             high_rank = db.get_highrank(con, idy)
-            if num < high_rank:
+            if high_rank is None or num < high_rank:
                 db.update_highrank(con, num, idy)
-            new_score = p.score
-            db.update_score(con, new_score, idy)
-        else:
-            # create new post entry
-            db.insert_new_post(con, p, num)
+                db.update_time_highrank(con, datetime.utcfromtimestamp(now), idy)
 
+    con.commit()
+
+
+def main():
+    while True:
+        ingest_new_posts(NEW_POST_LIMIT, SUB_NAME)
+        check_hot_posts(HOT_POST_LIMIT, SUB_NAME)
+        time.sleep(SLEEP_S)
+
+
+if __name__ == '__main__':
+    main()
