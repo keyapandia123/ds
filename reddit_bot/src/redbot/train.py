d1b0f253184253f140ae06af51db924da4c2b5f5
Keya Pandia <keyapandia123@gmail.com>
2021-08-15 14:10:47 -0700

    Documentation for train module.

--- reddit_bot/src/redbot/train.py
+++ reddit_bot/src/redbot/train.py
@@ -6,7 +6,6 @@ from lightgbm import LGBMClassifier
 import numpy as np
 import pandas as pd
 from sklearn import compose, feature_extraction, linear_model, metrics, model_selection
-import sqlite3
 from xgboost import XGBClassifier
 
 from redbot import db
@@ -16,9 +15,22 @@ DATABASE_DEFAULT_PATH = '~/.redbot/redbotdb.sqlite'
 
 
 def preprocess_valid_data(con=None):
+    """Replace urls with domain names and highrank24 with 'hot or not' indicator.
+
+    Generate a raw dataframe from all valid posts (created over 24 hours ago).
+    Preprocess the raw dataframe to generate a processed dataframe with
+    features (domain name, title, score, up_vote ratio) and a 'hot or not' indicator
+    for each valid post.
+
+    Args:
+        con: database connection.
+
+    Returns:
+        df: DataFrame. A processed dataframe with features (domain name, title, score, up_vote ratio)
+        and a 'hot or not' indicator for each valid post.
+    """
     if not con:
         con = db.connect_to_db(DATABASE_DEFAULT_PATH, create_if_empty=False)
-    cursor = con.cursor()
 
     current_time = time.time()
     current_time_utc = datetime.utcfromtimestamp(current_time)
@@ -51,6 +63,21 @@ def preprocess_valid_data(con=None):
 
 
 def create_train_test_split(df, random_sample=False):
+    """Create training and test features and labels for model training and testing.
+
+    Either create a randomized train-test split or a sequence-aware split
+    where first 80% valid posts are used for training and the latter 20% for testing.
+
+    Args:
+         df: DataFrame. Dataframe of features and 'hot or not' indicators for all valid posts.
+         random_sample: bool. If True, use randomized split; if False use sequence-aware split.
+
+    Returns:
+        x_train: DataFrame. DataFrame of features for training model.
+        y_train: Series. Series of labels for training model.
+        x_test: DataFrame. DataFrame of features for testing/evaluating model.
+        y_test: Series. Series of true labels for testing/evaluating model.
+    """
     if random_sample:
         train_idx, test_idx = model_selection.train_test_split(df.index, test_size=0.2, stratify=df['hot_or_not'])
     else:
@@ -72,6 +99,23 @@ def create_train_test_split(df, random_sample=False):
 
 
 def transform_text_columns(x_train, x_test):
+    """Transform textual features into numerical representation.
+
+    Use Tfidf vectorizer to transform titles and domains in train and test features
+    into numerical representation.  The remainder of the numerical features (scores, up_vote_ratios)
+    are used without any transformation.
+
+    Args:
+        x_train: DataFrame. DataFrame of features (combination of text features: titles and domains,
+        and numerical features: scores and up_vote ratios), for training posts.
+        x_test: DataFrame.  DataFrame of the same categories of features, for test posts.
+
+    Returns:
+        x_train_transformed: sparse matrix. For training posts, text features converted into sparse numerical matrix;
+        numerical features preserved unmodified.
+        x_test_transformed: sparse matrix. Same transformation applied to text features of test posts; numerical features
+        preserved unmodified.
+    """
     ct = compose.ColumnTransformer(
         [('titles_bow',
           feature_extraction.text.TfidfVectorizer(stop_words=['of', 'to', 'in', 'for', 'the', 'and', 'are', 'on',
@@ -87,6 +131,20 @@ def transform_text_columns(x_train, x_test):
 
 
 def build_and_verify_model(clf, x_train_tr, x_test_tr, y_train, y_test):
+    """Fit model using training data and verify performance of model on test data.
+
+    Args:
+        clf: estimator/classifier. Object used to fit the training data.
+        x_train_tr: sparse matrix. Training features used to train the classifier.
+        x_test_tr: sparse matrix. Test features used to evaluate fitted classifier.
+        y_train: Series. Training labels used to train the classifier.
+        y_test: Series. Test labels used to test the classifier.
+
+    Returns:
+        accuracy: float. Accuracy score evaluated on test data.
+        f1: float. F1-score evaluated on test data.
+        cm: numpy array. Confusion matrix evaluated on test data.
+    """
     clf.fit(x_train_tr, y_train)
     y_pred = clf.predict(x_test_tr)
 
@@ -102,6 +160,11 @@ def build_and_verify_model(clf, x_train_tr, x_test_tr, y_train, y_test):
 
 
 def run_and_evaluate_training():
+    """Comparative analysis of three types of classifiers.
+
+    Compare accuracy, F1-score, and confusion matrix for
+    Logistic Regression, XGBoost, and LightGBM Classifiers.
+    """
     df = preprocess_valid_data()
     x_train, y_train, x_test, y_test = create_train_test_split(df, random_sample=True)
     x_train_transformed, x_test_transformed = transform_text_columns(x_train, x_test)
@@ -111,7 +174,6 @@ def run_and_evaluate_training():
     accuracy, f1, cm = build_and_verify_model(clf1, x_train_transformed, x_test_transformed, y_train, y_test)
 
     print("\nXGBoost : ")
-    class_wts = {0: 1, 1: 1000}
     clf2 = XGBClassifier(booster='gbtree', max_depth=12, learning_rate=0.1, n_estimators=1000, use_label_encoder=False)
     accuracy, f1, cm = build_and_verify_model(clf2, x_train_transformed, x_test_transformed, y_train, y_test)
 
