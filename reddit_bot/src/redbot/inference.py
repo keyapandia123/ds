da5aedef199598acd3cb5338b52e9d69f775afe6
Keya Pandia <keyapandia123@gmail.com>
2021-08-18 17:40:35 -0700

    Add documentation for inference module.

--- reddit_bot/src/redbot/inference.py
+++ reddit_bot/src/redbot/inference.py
@@ -16,6 +16,17 @@ DATABASE_DEFAULT_PATH = '~/.redbot/redbotdb.sqlite'
 
 
 def create_transformer_and_model(df, to_save=False):
+    """Generate column transformer and classifier/estimator object fitted to all of the valid data.
+
+    Args:
+        df: DataFrame.  Preprocessed DataFrame with features and labels
+        obtained from the function preprocess_valid_data() of the train module.
+        to_save: bool. If True, save the estimator/classifier model and column transformer objects.
+
+    Returns:
+        ct: Fitted column transformer object
+        clf: Fitted estimator/classifier object
+    """
     y_train = df['hot_or_not']
     x_train = df.drop(['hot_or_not'], axis=1)
 
@@ -44,6 +55,19 @@ def create_transformer_and_model(df, to_save=False):
 
 
 def retrieve_hour_old_posts(con=None):
+    """Retrieve posts ingested between 1 and 3 hours ago that have no predictions.
+
+    Use posts that were ingested at least one hour prior to making predictions
+    so that the score (upvotes minus downvotes) for those posts has a valid stable value.
+
+    Args:
+        con: database connection.
+
+    Returns:
+        raw_test_df: DataFrame. DataFrame containing raw attributes
+        (id, url, title, score, upvote_ratio) for posts ingested
+        between 1 and 3 hours ago.
+    """
     if not con:
         con = db.connect_to_db(DATABASE_DEFAULT_PATH, create_if_empty=False)
 
@@ -61,6 +85,22 @@ def retrieve_hour_old_posts(con=None):
 
 
 def preprocess_hour_old_posts(raw_test_df):
+    """Preprocess posts that were ingested between 1 and 3 hours ago, for which there are no predictions.
+
+    Preprocess the raw attributes by replacing the url with the domain and dropping the primary key.
+    Generate a prediction table containing the primary keys of the posts.
+
+    Args:
+        raw_test_df: DataFrame containing raw attributes
+        (id, url, title, score, upvote_ratio) for posts ingested
+        between 1 and 3 hours ago.
+
+    Returns:
+        df: DataFrame. DataFrame containing features (domains, titles, scores, upvote_ratios)
+        of the hour old posts.
+        prediction_table: DataFrame. DataFrame containing ids (primary keys)
+        of the hour old posts.
+    """
     df = raw_test_df.copy()
     full_domain = []
     for idx in df.index:
@@ -80,6 +120,12 @@ def preprocess_hour_old_posts(raw_test_df):
 
 
 def save_predictions_to_table(prediction_table, con=None):
+    """Update the prediction made for the hour old posts to posts table in the database.
+
+    Args:
+        prediction_table: DataFrame. Contains the primary key and prediction for the hour old posts.
+        con: database connection.
+    """
     if not con:
         con = db.connect_to_db(DATABASE_DEFAULT_PATH, create_if_empty=False)
 
@@ -90,6 +136,22 @@ def save_predictions_to_table(prediction_table, con=None):
 
 
 def run_inference(new_model=True, to_save=False):
+    """Function to run inference on hour old posts.
+
+    Generate new model and column transformer objects or retrieve saved objects.
+    Transform text features of hour old posts to sparse numerical matrices using the column transformer object.
+    Run prediction on the generated matrix. Modify the prediction table to add a column of prediction values, one
+    for each post corresponding to each primary key.
+
+    Args:
+        new_model: bool. If True, generate new estimator/classifier and column transformer object.
+        If False, retrieve saved objects.
+        to_save: bool. If True, save generated estimator/classifier and column transformer objects.
+
+    Returns:
+        prediction_table: DataFrame. Contains a column of primary keys ("id") and a column of prediction values
+        ("predictions") for each retrieved hour old post.
+    """
     if new_model:
         df = train.preprocess_valid_data()
         ct, clf = create_transformer_and_model(df, to_save)
